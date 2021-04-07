9d97e2564e998f661559d6f845645a767fce1866
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-04-06 21:22:38 -0300

    del: delete the palette color

--- src/App.js
+++ src/App.js
@@ -6,50 +6,31 @@ import Experience from "./components/Experience";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar/NavBar";
 import Project from "./components/Project";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core";
-
 import "./index.css";
-
-const theme = createMuiTheme({
-	palette: {
-		primary: {
-			main: "#f7f7fe",
-			dark: "#1a1919",
-		},
-		secondary: {
-			main: "#100f10",
-		},
-		tertiary: {
-			main: "#37b9f1",
-		},
-	},
-});
 
 function App() {
 	return (
-		<ThemeProvider theme={theme}>
-			<Router>
-				<NavBar />
-				<Switch>
-					<Route exact path="/about">
-						<About />
-					</Route>
-					<Route exact path="/contact">
-						<Contact />
-					</Route>
-					<Route exact path="/">
-						<Home />
-					</Route>
-					<Route exact path="/experience">
-						<Experience />
-					</Route>
-					<Route exact path="/project">
-						<Project />
-					</Route>
-				</Switch>
-				<Background />
-			</Router>
-		</ThemeProvider>
+		<Router>
+			<NavBar />
+			<Switch>
+				<Route exact path="/about">
+					<About />
+				</Route>
+				<Route exact path="/contact">
+					<Contact />
+				</Route>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route exact path="/experience">
+					<Experience />
+				</Route>
+				<Route exact path="/project">
+					<Project />
+				</Route>
+			</Switch>
+			<Background />
+		</Router>
 	);
 }
 
