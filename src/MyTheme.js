70419da0a2a032852f4e29cefe0d6a558346b385
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-04-06 21:23:07 -0300

    add: add the palette color for this file

---
+++ src/MyTheme.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export default createMuiTheme({
+	palette: {
+		primary: {
+			main: "#f7f7fe",
+			dark: "#1a1919",
+		},
+		secondary: {
+			main: "#100f10",
+		},
+		accent: {
+			backgroundColor: "#37b9f1",
+			constrastText: "#fff",
+		},
+	},
+});
