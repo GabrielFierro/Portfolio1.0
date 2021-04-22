121ac8c9d5b10b8ba5b84001def1a61635fb5c1d
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-04-22 14:01:01 -0300

    add: add breakpoints to define the responsive text

--- src/components/Presentation/Presentation.js
+++ src/components/Presentation/Presentation.js
@@ -11,15 +11,43 @@ const useStyles = makeStyles((theme) => ({
 		height: "100%",
 		position: "absolute",
 		width: "50%",
+		[theme.breakpoints.down("xs")]: {
+			left: "15%",
+			width: "70%",
+		},
 	},
 	title: {
 		color: "rgba(16, 15, 16, 1)",
-		margin: "0 2rem 0 2rem",
+		margin: "0 1.5rem 0 1.5rem",
+		fontSize: "3.75rem",
+		letterSpacing: "-0.00833em",
+		lineHeight: "1.2",
 		position: "relative",
 		top: "30%",
+		[theme.breakpoints.down("md")]: {
+			fontSize: "3.2rem",
+			top: "25%",
+		},
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "2.6rem",
+			top: "20%",
+		},
+		[theme.breakpoints.down("xs")]: {
+			fontSize: "1.8rem",
+			top: "30%",
+		},
 	},
 	subtitle: {
 		color: "#100f10",
+		fontSize: "3.75rem",
+		letterSpacing: "-0.00833em",
+		lineHeight: "1.2",
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "2.6rem",
+		},
+		[theme.breakpoints.down("xs")]: {
+			fontSize: "1.8rem",
+		},
 	},
 }));
 
@@ -29,13 +57,12 @@ function Presentation() {
 		<article>
 			<ThemeProvider theme={MyTheme}>
 				<Grid item xs direction="column" className={classes.card}>
-					<Typography
-						className={classes.title}
-						style={MyTheme.typographyRubik}
-						variant="h2"
-					>
+					<Typography className={classes.title} style={MyTheme.typographyRubik}>
 						Hola, me llamo Gabriel Fierro.
-						<Typography style={MyTheme.typographyRubik} variant="h2">
+						<Typography
+							className={classes.subtitle}
+							style={MyTheme.typographyRubik}
+						>
 							Soy un desarrollador
 							<span style={MyTheme.palette.accentColor}> front-end </span>
 							creativo.
