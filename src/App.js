7349e55dbec94062d0807631ff3b668007f2954f
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-04-27 17:29:01 -0300

    fix: fix the import for the Experience, Home and Project components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import About from "./components/About/About";
 import Background from "./components/Background/Background";
 import Contact from "./components/Contact/Contact";
-import Experience from "./components/Experience";
-import Home from "./components/Home";
+import Experience from "./components/Experience/Experience";
+import Home from "./components/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
-import Project from "./components/Project";
+import Project from "./components/Project/Project";
 import "./index.css";
 
 function App() {
