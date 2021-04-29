2c5933f3ea65424ff5e9ed4e72d8fb96cc9498da
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-04-28 22:54:24 -0300

    del: delete the imports makeStyles and for each logo that was unused

--- src/components/About/Logo/Logo.js
+++ src/components/About/Logo/Logo.js
@@ -1,14 +1,5 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
-import HtmlLogo from "../../../assets/images/icons/html-logo.svg";
-import CssLogo from "../../../assets/images/icons/css-logo.svg";
-import TailwindLogo from "../../../assets/images/icons/tailwind-logo.svg";
-import BootstrapLogo from "../../../assets/images/icons/bootstrap-logo.svg";
-import PythonLogo from "../../../assets/images/icons/python-logo.svg";
-import FlaskLogo from "../../../assets/images/icons/flask-logo.svg";
-import JavascriptLogo from "../../../assets/images/icons/javascript-logo.svg";
-import GitLogo from "../../../assets/images/icons/git-icon.svg";
 
 function Logo(props) {
 	return (
