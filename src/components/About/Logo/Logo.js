3d9ba8ed46fbdfaeaa1201c4691c7b6848c332a6
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-04-28 22:51:00 -0300

    delete the internal styles and add props for the alt and src properties to reduce the component

--- src/components/About/Logo/Logo.js
+++ src/components/About/Logo/Logo.js
@@ -10,44 +10,11 @@ import FlaskLogo from "../../../assets/images/icons/flask-logo.svg";
 import JavascriptLogo from "../../../assets/images/icons/javascript-logo.svg";
 import GitLogo from "../../../assets/images/icons/git-icon.svg";
 
-const useStyles = makeStyles((theme) => ({
-	margin: {
-		margin: "15px 0 0 15px",
-	},
-}));
-
-function Logo() {
-	const classes = useStyles();
+function Logo(props) {
 	return (
-		<Box
-			alignItems="flex-start"
-			display="flex"
-			p={1}
-			style={{ marginLeft: "90px" }}
-		>
-			<Box style={{ marginTop: "5px" }}>
-				<img alt="html logo" src={HtmlLogo}></img>
-			</Box>
-			<Box style={{ marginTop: "5px" }}>
-				<img alt="css logo" src={CssLogo}></img>
-			</Box>
-			<Box style={{ marginTop: "20px" }}>
-				<img alt="tailwind logo" src={TailwindLogo}></img>
-			</Box>
-			<Box className={classes.margin}>
-				<img alt="bootstrap logo" src={BootstrapLogo}></img>
-			</Box>
-			<Box className={classes.margin}>
-				<img alt="python logo" src={PythonLogo}></img>
-			</Box>
-			<Box className={classes.margin}>
-				<img alt="flask logo" src={FlaskLogo}></img>
-			</Box>
-			<Box className={classes.margin}>
-				<img alt="javascript logo" src={JavascriptLogo}></img>
-			</Box>
-			<Box className={classes.margin}>
-				<img alt="git logo" src={GitLogo}></img>
+		<Box alignItems="flex-start" display="flex" p={1}>
+			<Box>
+				<img alt={props.alt} src={props.src}></img>
 			</Box>
 		</Box>
 	);
