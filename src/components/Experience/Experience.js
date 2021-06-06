41b65247078e52706084b7af549022e8c3e5cf02
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-06-05 21:20:26 -0300

    delete the timeline, the footer and add properties to make responsive components

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,68 +1,95 @@
 /* Experience.js */
-import Container from "@material-ui/core/Container";
-import { makeStyles } from "@material-ui/core/styles";
-import { Box, Grid, Typography, ThemeProvider } from "@material-ui/core";
+import {
+	Box,
+	Button,
+	Grid,
+	makeStyles,
+	Typography,
+	ThemeProvider,
+} from "@material-ui/core";
 import Card from "../Card/Card";
 import MyTheme from "../../MyTheme";
-import Timeline from "../../assets/images/timeline/timeline.svg";
 import Magenta from "../../assets/images/magenta/magenta.png";
-import HtmlLogo from "../../assets/images/icons/html-logo.svg";
-import CssLogo from "../../assets/images/icons/css-logo.svg";
-import TailwindLogo from "../../assets/images/icons/tailwind-logo.svg";
-import JavascriptLogo from "../../assets/images/icons/javascript-logo.svg";
+import MagentaMobile from "../../assets/images/magenta/magenta_mobile.avif";
 import ButtonPrimary from "../Buttons/ButtonPrimary";
 import ButtonSecondary from "../Buttons/ButtonSecondary";
 
 const useStyles = makeStyles((theme) => ({
 	title: {
-		left: "12%",
-		position: "absolute",
-		top: "20%",
-		width: "75%",
+		position: "relative",
+		width: "100%",
 	},
 	skills_text: {
 		alignItems: "center",
 		display: "flex",
 		fontSize: "3rem",
 		justifyContent: "center",
+		[theme.breakpoints.down("md")]: {
+			fontSize: "2.65rem",
+		},
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "2.35rem",
+		},
+		[theme.breakpoints.down("xs")]: {
+			fontSize: "2rem",
+		},
 	},
 	project_name: {
 		alignItems: "center",
 		display: "flex",
 		fontSize: "1.75rem",
 		justifyContent: "center",
-		marginTop: "-10px",
+		[theme.breakpoints.down("md")]: {
+			fontSize: "1.5rem",
+		},
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "1.25rem",
+			marginLeft: "10px",
+		},
+		[theme.breakpoints.down("xs")]: {
+			fontSize: "1rem",
+			marginLeft: "0px",
+		},
 	},
 	project_name_description: {
 		alignItems: "center",
 		display: "flex",
 		fontSize: "1rem",
 		justifyContent: "center",
-		width: "95%",
-		marginLeft: "-10px",
+		width: "100%",
 	},
 	description_text: {
 		fontSize: "1.25rem",
-		margin: "0 30px 0 60px",
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "1rem",
+		},
+		[theme.breakpoints.down("xs")]: {
+			fontSize: "0.75rem",
+		},
 	},
 	description_container: {
-		height: "45%",
+		height: "70%",
 		left: "15%",
 		position: "absolute",
-		top: "30%",
+		top: "20%",
 		width: "70%",
 	},
 	left_container: {
-		height: "100%",
-		paddingTop: "20px",
+		height: "50%",
+		left: "15%",
 		position: "absolute",
-		width: "50%",
+		top: "35%",
+		width: "35%",
+		[theme.breakpoints.down("xs")]: {
+			top: "30%",
+		},
 	},
 	right_container: {
-		height: "100%",
-		left: "50%",
+		height: "50%",
+		left: "52%",
 		position: "absolute",
-		width: "50%",
+		top: "30%",
+		width: "35%",
 	},
 	footer_container: {
 		height: "12%",
@@ -72,18 +99,81 @@ const useStyles = makeStyles((theme) => ({
 		width: "70%",
 	},
 	project_image: {
-		width: "100%",
-		margin: "10px 0 0 35px",
+		width: "90%",
+		margin: "10px 0 0 5px",
 	},
 	image: {
-		width: "380px",
+		width: "100%",
 		margin: "5px 0 10px 0",
 		borderRadius: "5px",
-		boxShadow: "1px 1px 8px #100f10",
+		[theme.breakpoints.down("sm")]: {
+			marginLeft: "-10px",
+		},
 	},
 	button: {
-		width: "420px",
-		margin: "5px 0 10px 25px",
+		width: "80%",
+		margin: "5px 0 5px 0",
+		[theme.breakpoints.down("xs")]: {
+			margin: "5px 0 5px -20px",
+		},
+	},
+	button_container: {
+		width: "100%",
+		height: "50%",
+	},
+	button_image: {
+		width: "40%",
+		margin: "5px",
+	},
+	informative_button: {
+		width: "100%",
+		boxShadow: "1px 1px 8px #100f10",
+	},
+	button_primary: {
+		width: "90%",
+		boxShadow: "1px 1px 8px #100f10",
+		"&:hover": {
+			boxShadow: "3px 3px 10px #37b9f1",
+		},
+	},
+	button_secondary: {
+		width: "90%",
+		boxShadow: "1px 1px 8px #100f10",
+		"&:hover": {
+			boxShadow: "3px 3px 10px #37b9f1",
+		},
+	},
+	button_text: {
+		fontSize: "1.25rem",
+		color: "#f7f7fe",
+		[theme.breakpoints.down("md")]: {
+			fontSize: "0.75rem",
+		},
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "0.6rem",
+		},
+	},
+	button_text_primary: {
+		color: "#f7f7fe",
+		fontSize: "1.25rem",
+		paddingTop: "3px",
+		[theme.breakpoints.down("md")]: {
+			fontSize: "0.75rem",
+		},
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "0.5rem",
+			paddingTop: "0px",
+		},
+	},
+	button_text_secondary: {
+		fontSize: "1.25rem",
+		color: "#100f10",
+		[theme.breakpoints.down("md")]: {
+			fontSize: "0.75rem",
+		},
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "0.5rem",
+		},
 	},
 	primary_button: {
 		marginLeft: "15px",
@@ -92,10 +182,6 @@ const useStyles = makeStyles((theme) => ({
 	primary_button_text: {
 		color: "#f7f7fe",
 		fontSize: "24px",
-	},
-	timeline: {
-		width: "5%",
-		marginTop: "15px",
 	},
 	icon: {
 		marginLeft: "15px",
@@ -108,6 +194,12 @@ const useStyles = makeStyles((theme) => ({
 		fontSize: "1.25rem",
 		marginTop: "5px",
 	},
+	grid_container: {
+		zIndex: "1",
+		height: "500px",
+		position: "relative",
+		width: "500px",
+	},
 }));
 
 function Experience() {
@@ -115,17 +207,19 @@ function Experience() {
 	return (
 		<ThemeProvider theme={MyTheme}>
 			<Card />
-			<Container maxWidth="sm">
-				<Box className={classes.title}>
-					<Typography
-						className={classes.skills_text}
-						gutterbottom
-						style={MyTheme.typographyRubik}
-					>
-						Experiencia
-					</Typography>
-				</Box>
-				<Box className={classes.description_container}>
+			<Grid spacing={3}>
+				<Grid item xs={12} className={classes.description_container}>
+					<Box className={classes.title}>
+						<Typography
+							className={classes.skills_text}
+							gutterbottom
+							style={MyTheme.typographyRubik}
+						>
+							Experiencia
+						</Typography>
+					</Box>
+				</Grid>
+				<Grid item xs={6}>
 					<Grid className={classes.left_container}>
 						<Typography
 							className={classes.project_name}
@@ -133,18 +227,20 @@ function Experience() {
 						>
 							Code in magenta
 						</Typography>
-						<Grid container xs={2} sm direction="row">
-							<Grid direction="row" className={classes.timeline}>
-								<Box>
-									<img alt="timeline with circles" src={Timeline}></img>
-								</Box>
-							</Grid>
+						<Grid container direction="row">
 							<Grid
 								direction="column"
 								className={classes.project_name_description}
 							>
 								<Grid className={classes.button}>
-									<ButtonPrimary name="Octubre 2020 - Noviembre 2020"></ButtonPrimary>
+									<Button
+										className={classes.informative_button}
+										style={MyTheme.palette.buttonPrimary}
+									>
+										<Typography className={classes.button_text}>
+											Octubre 2020 - Noviembre 2020
+										</Typography>
+									</Button>
 								</Grid>
 								<Typography
 									className={classes.description_text}
@@ -157,63 +253,59 @@ function Experience() {
 							</Grid>
 						</Grid>
 					</Grid>
-					<Grid className={classes.right_container}>
-						<Box className={classes.project_image}>
-							<img
-								alt="magenta project"
-								className={classes.image}
-								src={Magenta}
-							></img>
-						</Box>
-						<Box alignItems="flex-start" display="flex" p={1}>
-							<Grid className={classes.primary_button}>
-								<ButtonPrimary name="Demo"></ButtonPrimary>
-							</Grid>
-							<Grid className={classes.secondary_button}>
-								<ButtonSecondary name="Repositorio" />
-							</Grid>
-						</Box>
-					</Grid>
-				</Box>
-				<Grid direction="row" className={classes.footer_container}>
-					<Box alignItems="flex-start" display="flex" p={1}>
-						<Typography
-							className={classes.develop_text}
-							style={MyTheme.typographyRubik}
-						>
-							Desarrollado con:
-						</Typography>
-						<Box className={classes.icon}>
-							<img
-								alt="html logo"
-								className={classes.icon_size}
-								src={HtmlLogo}
-							></img>
-						</Box>
-						<Box className={classes.icon}>
-							<img
-								alt="css logo"
-								className={classes.icon_size}
-								src={CssLogo}
-							></img>
-						</Box>
-						<Box className={classes.icon}>
-							<img
-								alt="tailwind logo"
-								className={classes.icon_size}
-								src={TailwindLogo}
-							></img>
-						</Box>
-						<Box className={classes.icon}>
-							<img
-								alt="javascript logo"
-								className={classes.icon_size}
-								src={JavascriptLogo}
-							></img>
-						</Box>
-					</Box>
 				</Grid>
-			</Container>
+				<Grid item xs={6} className={classes.right_container}>
+					<Grid container>
+						<Grid className={classes.project_image}>
+							<picture>
+								<source media="(min-width:500px)" srcset={Magenta}></source>
+								<source
+									media="(min-width:200px)"
+									srcset={MagentaMobile}
+								></source>
+								<img
+									alt="magenta project"
+									className={classes.image}
+									src={Magenta}
+								></img>
+							</picture>
+						</Grid>
+						<Grid
+							className={classes.button_container}
+							container
+							direction="row"
+							xs={12}
+						>
+							<Grid className={classes.button_image} item xs={6} sm={5}>
+								<Button
+									className={classes.button_primary}
+									href={"https://magentateam.github.io/landingOng/public/"}
+									target="_blank"
+									rel="noopener noreferrer"
+									style={MyTheme.palette.buttonPrimary}
+								>
+									<Typography className={classes.button_text_primary}>
+										Demo
+									</Typography>
+								</Button>
+							</Grid>
+							<Grid className={classes.button_image} item xs={6} sm={5}>
+								<Button
+									className={classes.button_secondary}
+									href={"https://github.com/magentateam/landingOng"}
+									target="_blank"
+									rel="noopener noreferrer"
+									style={MyTheme.palette.buttonSecondary}
+								>
+									<Typography className={classes.button_text_secondary}>
+										Repositorio
+									</Typography>
+								</Button>
+							</Grid>
+						</Grid>
+					</Grid>
+				</Grid>
+			</Grid>
 		</ThemeProvider>
 	);
 }
