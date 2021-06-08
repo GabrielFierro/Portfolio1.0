8ec265961d0ab7e83e802c97e4e8316bd8ae72e9
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-06-08 16:45:11 -0300

    delete unused external css and add the internal css in the Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,23 @@
 /* Card.js */
-import "./Card.css";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+	sectionCard: {
+		background: theme.palette.primary.main,
+		borderRadius: "7px",
+		height: "75%",
+		left: "12%",
+		opacity: "0.9",
+		position: "absolute",
+		top: "17%",
+		width: "75%",
+	},
+}));
 
 function Card() {
-	return <div className="section-card"></div>;
+	const classes = useStyles();
+
+	return <div className={classes.sectionCard}></div>;
 }
 
 export default Card;
