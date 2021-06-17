07b989c25e646467c41fbe61799bd8f4fa913103
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-06-17 18:26:31 -0300

    add the imports for useState and emailjs, fix the imports for icons and deleted the imports for buttons, add the styles and the logic for submit or reset the form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,6 @@
 /* Contact.js */
+import { useState } from "react";
+import emailjs from "emailjs-com";
 import "./Contact.css";
 import Card from "../Card/Card";
 import MyTheme from "../../MyTheme";
@@ -11,13 +13,26 @@ import {
 	ThemeProvider,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import ButtonPrimary from "../Buttons/ButtonPrimary";
-import ButtonSecondary from "../Buttons/ButtonSecondary";
-import LinkedInIcon from "./Screens/LinkedInIcon/LinkedInIcon.js";
-import GitHubIcon from "./Screens/GitHubIcon/GitHubIcon.js";
-import TwitterIcon from "./Screens/TwitterIcon/TwitterIcon.js";
+import GitHubIcon from "./Screens/GitHubIcon/GitHubIcon";
+import LinkedInIcon from "./Screens/LinkedInIcon/LinkedInIcon";
+import TwitterIcon from "./Screens/TwitterIcon/TwitterIcon";
+import LinkIcon from "./Screens/LinkIcon/LinkIcon";
 
 const useStyles = makeStyles((theme) => ({
+	section: {
+		position: "absolute",
+		width: "60%",
+		left: "19%",
+		top: "15%",
+	},
+	iconContainer: {
+		border: "2px solid #100f10",
+		marginBottom: "2rem",
+		position: "relative",
+		width: "100%",
+		backgroundColor: MyTheme.palette.primary.main,
+		height: "60px",
+	},
 	textFieldName: {
 		width: "46%",
 		marginBottom: "0.5em",
@@ -59,35 +74,58 @@ const useStyles = makeStyles((theme) => ({
 		marginTop: "5px",
 		marginBottom: "15px",
 	},
-	icon: {
+	iconSpace: {
 		width: "50px",
 		height: "50px",
 		marginLeft: "30px",
 		marginTop: "5px",
 		[theme.breakpoints.down("sm")]: {
+			width: "45px",
+			height: "45px",
+			marginLeft: "25px",
+		},
+		[theme.breakpoints.down("xs")]: {
 			width: "40px",
 			height: "40px",
+			marginLeft: "20px",
+		},
+	},
+	icon: {
+		width: "50px",
+		height: "50px",
+		"&:hover": {
+			boxShadow: "3px 3px 10px #37b9f1",
+		},
+		[theme.breakpoints.down("sm")]: {
+			marginTop: "5px",
+			width: "45px",
+			height: "45px",
 			marginLeft: "25px",
+		},
+		[theme.breakpoints.down("xs")]: {
+			width: "40px",
+			height: "40px",
+			marginLeft: "20px",
 		},
 	},
 	linkIcon: {
-		width: "55px",
-		height: "55px",
+		width: "50px",
+		height: "50px",
+		marginTop: "4px",
 		marginLeft: "30px",
-		marginTop: "5px",
 		[theme.breakpoints.down("sm")]: {
 			marginLeft: "20px",
-			marginTop: "7px",
+			marginTop: "4px",
 		},
 	},
 	gridMarginLeft: {
-		marginLeft: "20px",
+		marginLeft: "0px",
 		[theme.breakpoints.down("xs")]: {
 			marginLeft: "10px",
 		},
 	},
 	form: {
-		backgroundColor: "#f7f7fe",
+		backgroundColor: MyTheme.palette.primary.main,
 		border: "2px solid #100f10",
 		width: "100%",
 		[theme.breakpoints.down("md")]: {
@@ -114,71 +152,116 @@ const useStyles = makeStyles((theme) => ({
 			marginLeft: "0.5em",
 		},
 	},
+	skills_text: {
+		marginTop: "10px",
+		fontSize: "3rem",
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		[theme.breakpoints.down("md")]: {
+			fontSize: "2.65rem",
+		},
+		[theme.breakpoints.down("sm")]: {
+			marginTop: "15px",
+			fontSize: "2.35rem",
+		},
+		[theme.breakpoints.down("xs")]: {
+			fontSize: "2rem",
+			marginTop: "20px",
+		},
+	},
+	send_button: {
+		backgroundColor: "#37b9f1",
+		border: "2px solid #37b9f1",
+		borderRadius: "5px",
+		boxShadow: "1px 1px 8px #100f10",
+		color: "#f7f7fe",
+		cursor: "pointer",
+		fontSize: "20px",
+		marginLeft: "20px",
+		padding: "7px",
+		width: "15%",
+		"&:hover": {
+			boxShadow: "3px 3px 10px #37b9f1",
+		},
+		[theme.breakpoints.down("md")]: {
+			width: "20%",
+		},
+		[theme.breakpoints.down("sm")]: {
+			width: "25%",
+		},
+		[theme.breakpoints.down("xs")]: {
+			width: "30%",
+		},
+	},
+	cancel_button: {
+		backgroundColor: "#f7f7fe",
+		border: "2px solid #37b9f1",
+		borderRadius: "5px",
+		color: "#100f10",
+		cursor: "pointer",
+		fontSize: "20px",
+		padding: "7px",
+		width: "15%",
+		boxShadow: "1px 1px 8px #100f10",
+		marginLeft: "20px",
+		"&:hover": {
+			boxShadow: "3px 3px 10px #37b9f1",
+		},
+		[theme.breakpoints.down("md")]: {
+			width: "20%",
+		},
+		[theme.breakpoints.down("sm")]: {
+			width: "25%",
+		},
+		[theme.breakpoints.down("xs")]: {
+			width: "35%",
+		},
+	},
 }));
 
 function Contact() {
 	const classes = useStyles();
 	const preventDefault = (event) => event.preventDefault();
+
+	function sendMessage(e) {
+		e.preventDefault();
+
+		emailjs
+			.sendForm(
+				"service_oqksey9",
+				"template_9kr5zyo",
+				e.target,
+				"user_DDXa2vjByng4wMUXQi3By"
+			)
+			.then(
+				(result) => {
+					alert("Tu información se ha enviado exitosamente!");
+					e.target.reset();
+					console.log(result.text);
+				},
+				(error) => {
+					console.log(error.text);
+				}
+			);
+	}
+
 	return (
 		<ThemeProvider theme={MyTheme}>
 			<Card />
-			<div class="section">
+			<Grid className={classes.section} container direction="column">
 				<Typography
-					class="text"
-					align="center"
-					gutterBottom
+					className={classes.skills_text}
 					style={MyTheme.typographyRubik}
 				>
 					Contacto
 				</Typography>
-				<Grid class="icon-container">
-					<div className={classes.linkIcon}>
-						<svg
-							class="icon-hover"
-							viewBox="0 0 65 65"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<circle cx="32.5" cy="32.5" r="32.5" fill="#1A1919" />
-							<path
-								d="M19 39.5C22.0376 39.5 24.5 37.0376 24.5 34C24.5 30.9624 22.0376 28.5 19 28.5C15.9624 28.5 13.5 30.9624 13.5 34C13.5 37.0376 15.9624 39.5 19 39.5Z"
-								stroke="#F7F7FE"
-								stroke-width="2.75"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							/>
-							<path
-								d="M41 28.5C44.0376 28.5 46.5 26.0376 46.5 23C46.5 19.9624 44.0376 17.5 41 17.5C37.9624 17.5 35.5 19.9624 35.5 23C35.5 26.0376 37.9624 28.5 41 28.5Z"
-								stroke="#F7F7FE"
-								stroke-width="2.75"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							/>
-							<path
-								d="M41 50.5C44.0376 50.5 46.5 48.0376 46.5 45C46.5 41.9624 44.0376 39.5 41 39.5C37.9624 39.5 35.5 41.9624 35.5 45C35.5 48.0376 37.9624 50.5 41 50.5Z"
-								stroke="#F7F7FE"
-								stroke-width="2.75"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							/>
-							<path
-								d="M23.95 31.6166L36.05 25.3833"
-								stroke="#F7F7FE"
-								stroke-width="2.75"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							/>
-							<path
-								d="M23.95 36.3833L36.05 42.6166"
-								stroke="#F7F7FE"
-								stroke-width="2.75"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							/>
-						</svg>
-					</div>
-					<Grid className={classes.gridMarginLeft} container>
-						<Grid className={classes.icon}>
+				<Grid container direction="column" className={classes.iconContainer}>
+					<Grid container className={classes.linkIcon}>
+						<LinkIcon />
+					</Grid>
+					<Grid container>
+						<Grid container className={classes.iconSpace}>
 							<Link
 								href="https://www.linkedin.com/in/gabriel-fierro-2020/"
 								onClick={preventDefault}
@@ -186,7 +269,7 @@ function Contact() {
 								<LinkedInIcon className={classes.icon} />
 							</Link>
 						</Grid>
-						<Grid className={classes.icon}>
+						<Grid container className={classes.iconSpace}>
 							<Link
 								href="https://github.com/GabrielFierro/"
 								onClick={preventDefault}
@@ -194,7 +277,7 @@ function Contact() {
 								<GitHubIcon className={classes.icon} />
 							</Link>
 						</Grid>
-						<Grid className={classes.icon}>
+						<Grid container className={classes.iconSpace}>
 							<Link
 								href="https://twitter.com/f_gabriel01"
 								onClick={preventDefault}
@@ -206,63 +289,75 @@ function Contact() {
 				</Grid>
 				<form
 					className={classes.form}
-					item
 					container
 					direction="column"
 					alignItems="center"
 					justify="center"
+					onSubmit={sendMessage}
 				>
 					<TextField
 						className={classes.textFieldName}
 						id="custom-name-field"
 						placeholder="Nombre"
-						maxlength="30"
+						minLength="3"
+						maxLength="30"
 						required
 						size="small"
 						style={{ marginRight: "0.7em", border: "1px solid #100f10" }}
 						testID="change-placeholder-color"
 						type="text"
 						variant="outlined"
+						name="name"
 					/>
 					<TextField
 						className={classes.textFieldSurname}
 						id="custom-surname-field"
 						placeholder="Apellido"
+						minLength="2"
 						maxlength="30"
 						required
 						size="small"
 						style={{ border: "1px solid #100f10" }}
 						type="text"
 						variant="outlined"
+						name="surname"
 					/>
 					<TextField
 						className={classes.textFieldInput}
 						id="custom-email-field"
 						placeholder="Email"
+						minLength="12"
 						maxlength="50"
 						required
 						size="small"
 						type="email"
 						variant="outlined"
+						name="email"
 					/>
 					<TextField
 						className={classes.textFieldInput}
 						id="custom-message-field"
 						placeholder="Mensaje"
-						maxlength="10"
+						minLength="10"
 						required
 						type="text"
 						variant="outlined"
+						name="feedback"
 					/>
 					<Box className={classes.button_container}>
-						<ButtonPrimary className={classes.send_button} name="Enviar" />
-						<ButtonSecondary
+						<input
+							type="submit"
+							className={classes.send_button}
+							value="Enviar"
+						/>
+						<input
+							type="reset"
 							className={classes.cancel_button}
-							name="Cancelar"
+							value="Cancelar"
 						/>
 					</Box>
 				</form>
-			</div>
+			</Grid>
 		</ThemeProvider>
 	);
 }
