1412a1f473cd0a802fdb2dca7899df8166f7aee5
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-06-17 17:57:59 -0300

    change the div component by a Grid component and fix the properties for the svg

--- src/components/Contact/Screens/LinkedInIcon/LinkedInIcon.js
+++ src/components/Contact/Screens/LinkedInIcon/LinkedInIcon.js
@@ -1,9 +1,10 @@
 /* TwitterIcon.js */
 import "../../Contact.css";
+import { Grid } from "@material-ui/core";
 
 function LinkedInIcon() {
 	return (
-		<div class="icon-hover">
+		<Grid class="icon-hover">
 			<svg
 				width="55"
 				height="55"
@@ -14,40 +15,40 @@ function LinkedInIcon() {
 				<path
 					d="M41.25 9.16675H13.75C11.2187 9.16675 9.16667 11.2188 9.16667 13.7501V41.2501C9.16667 43.7814 11.2187 45.8334 13.75 45.8334H41.25C43.7813 45.8334 45.8333 43.7814 45.8333 41.2501V13.7501C45.8333 11.2188 43.7813 9.16675 41.25 9.16675Z"
 					stroke="#100F10"
-					stroke-width="2.75"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+					strokeWidth="2.75"
+					strokeLinecap="round"
+					strokeLinejoin="round"
 				/>
 				<path
 					d="M18.3333 25.2083V36.6666"
 					stroke="#100F10"
-					stroke-width="2.75"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+					strokeWidth="2.75"
+					strokeLinecap="round"
+					strokeLinejoin="round"
 				/>
 				<path
 					d="M18.3333 18.3333V18.3562"
 					stroke="#100F10"
-					stroke-width="2.75"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+					strokeWidth="2.75"
+					strokeLinecap="round"
+					strokeLinejoin="round"
 				/>
 				<path
 					d="M27.5 36.6666V25.2083"
 					stroke="#100F10"
-					stroke-width="2.75"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+					strokeWidth="2.75"
+					strokeLinecap="round"
+					strokeLinejoin="round"
 				/>
 				<path
 					d="M36.6667 36.6666V29.7916C36.6667 28.576 36.1838 27.4102 35.3242 26.5507C34.4647 25.6911 33.2989 25.2083 32.0833 25.2083C30.8678 25.2083 29.702 25.6911 28.8424 26.5507C27.9829 27.4102 27.5 28.576 27.5 29.7916"
 					stroke="#100F10"
-					stroke-width="2.75"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+					strokeWidth="2.75"
+					strokeLinecap="round"
+					strokeLinejoin="round"
 				/>
 			</svg>
-		</div>
+		</Grid>
 	);
 }
 
