d67771d02b11cfe7a657bd2c5ecfb9dfb38dbae2
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-06-27 18:52:08 -0300

    add: add the dark mode

--- src/components/Presentation/Presentation.js
+++ src/components/Presentation/Presentation.js
@@ -1,4 +1,3 @@
-import "./Presentation.css";
 import MyTheme from "../../MyTheme";
 import { ThemeProvider } from "@material-ui/core";
 import { Typography } from "@material-ui/core";
@@ -6,7 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
-	card: {
+	cardLight: {
 		backgroundColor: MyTheme.palette.primary.main,
 		height: "100%",
 		position: "absolute",
@@ -16,7 +15,19 @@ const useStyles = makeStyles((theme) => ({
 			width: "70%",
 		},
 	},
-	title: {
+	cardDark: {
+		backgroundColor: MyTheme.palette.primary.dark,
+		height: "100%",
+		position: "absolute",
+		width: "50%",
+		left: "50%",
+		[theme.breakpoints.down("xs")]: {
+			left: "15%",
+			width: "70%",
+		},
+	},
+	titleLight: {
+		color: "#100f10",
 		letterSpacing: "-0.00833em",
 		lineHeight: "1.5",
 		margin: "-2rem 1.5rem 0 1.5rem",
@@ -36,8 +47,45 @@ const useStyles = makeStyles((theme) => ({
 			margin: "1rem 1.25rem 0 1rem",
 		},
 	},
-	subtitle: {
+	titleDark: {
+		color: "#f7f7fe",
+		letterSpacing: "-0.00833em",
+		lineHeight: "1.5",
+		margin: "-2rem 1.5rem 0 1.5rem",
+		position: "relative",
+		top: "30%",
+		[theme.breakpoints.down("md")]: {
+			fontSize: "3rem",
+			top: "25%",
+		},
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "2.75rem",
+			margin: "0 0.5rem 0 0.5rem",
+			top: "20%",
+		},
+		[theme.breakpoints.down("xs")]: {
+			fontSize: "2rem",
+			margin: "1rem 1.25rem 0 1rem",
+		},
+	},
+	subtitleLight: {
 		color: "#100f10",
+		display: "block",
+		letterSpacing: "-0.00833em",
+		lineHeight: "1.5",
+		[theme.breakpoints.down("md")]: {
+			fontSize: "3rem",
+		},
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "2.75rem",
+		},
+		[theme.breakpoints.down("xs")]: {
+			fontSize: "2rem",
+			margin: "1rem 1.25rem 0 0",
+		},
+	},
+	subtitleDark: {
+		color: "#f7f7fe",
 		display: "block",
 		letterSpacing: "-0.00833em",
 		lineHeight: "1.5",
@@ -63,38 +111,82 @@ const useStyles = makeStyles((theme) => ({
 	},
 }));
 
-function Presentation() {
+function Presentation(props) {
 	const classes = useStyles();
+	const darkMode = props.darkMode;
+
 	return (
 		<article>
 			<ThemeProvider theme={MyTheme}>
-				<Grid container item xs direction="column" className={classes.card}>
-					<Typography
-						variant="h2"
-						className={classes.title}
-						style={MyTheme.typographyRubik}
+				{darkMode ? (
+					<Grid
+						container
+						item
+						xs
+						direction="column"
+						className={classes.cardDark}
 					>
-						Hola, me llamo Gabriel Fierro.
 						<Typography
-							component="span"
 							variant="h2"
-							className={classes.subtitle}
+							className={classes.titleDark}
 							style={MyTheme.typographyRubik}
 						>
-							Soy un desarrollador
+							Hola, me llamo Gabriel Fierro.
 							<Typography
-								variant="h2"
 								component="span"
-								className={classes.spanColor}
+								variant="h2"
+								className={classes.subtitleDark}
 								style={MyTheme.typographyRubik}
 							>
-								{" "}
-								front-end{" "}
+								Soy un desarrollador
+								<Typography
+									variant="h2"
+									component="span"
+									className={classes.spanColor}
+									style={MyTheme.typographyRubik}
+								>
+									{" "}
+									front-end{" "}
+								</Typography>
+								creativo.
 							</Typography>
-							creativo.
 						</Typography>
-					</Typography>
-				</Grid>
+					</Grid>
+				) : (
+					<Grid
+						container
+						item
+						xs
+						direction="column"
+						className={classes.cardLight}
+					>
+						<Typography
+							variant="h2"
+							className={classes.titleLight}
+							style={MyTheme.typographyRubik}
+						>
+							Hola, me llamo Gabriel Fierro.
+							<Typography
+								component="span"
+								variant="h2"
+								className={classes.subtitleLight}
+								style={MyTheme.typographyRubik}
+							>
+								Soy un desarrollador
+								<Typography
+									variant="h2"
+									component="span"
+									className={classes.spanColor}
+									style={MyTheme.typographyRubik}
+								>
+									{" "}
+									front-end{" "}
+								</Typography>
+								creativo.
+							</Typography>
+						</Typography>
+					</Grid>
+				)}
 			</ThemeProvider>
 		</article>
 	);
