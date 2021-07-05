fe69db9edd9e30745adff6f2e291310c6eb69b75
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-07-05 20:34:34 -0300

    fix: fix the width and the position for the card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,24 +3,24 @@ import { makeStyles } from "@material-ui/core";
 import { Grid } from "@material-ui/core";
 import MyTheme from "../../MyTheme";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
 	sectionCardLight: {
 		backgroundColor: MyTheme.palette.primary.main,
 		borderRadius: "7px",
 		height: "75%",
-		left: "12%",
+		left: "10%",
 		position: "absolute",
 		top: "17%",
-		width: "75%",
+		width: "80%",
 	},
 	sectionCardDark: {
 		backgroundColor: MyTheme.palette.primary.dark,
 		borderRadius: "7px",
 		height: "75%",
-		left: "12%",
+		left: "10%",
 		position: "absolute",
 		top: "17%",
-		width: "75%",
+		width: "80%",
 	},
 }));
 
