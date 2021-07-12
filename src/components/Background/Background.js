33ab07b8834a462690a64f21b302cda0509de0e8
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-07-12 20:07:06 -0300

    add: add the images for the background on mobile devices

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -3,6 +3,8 @@ import { makeStyles } from "@material-ui/core";
 /* Background.js */
 import BackgroundLight from "../../assets/images/background/city-light-desktop.jpg";
 import BackgroundDark from "../../assets/images/background/city-dark-desktop.jpg";
+import BackgroundLightMobile from "../../assets/images/background/city-light-mobile.jpg";
+import BackgroundDarkMobile from "../../assets/images/background/city-dark-mobile.jpg";
 
 const useStyles = makeStyles((theme) => ({
 	container: {
@@ -11,7 +13,7 @@ const useStyles = makeStyles((theme) => ({
 		width: "100%",
 	},
 	background: {
-		height: "100%",
+		height: "100vh",
 		objectFit: "cover",
 		position: "relative",
 		width: "100%",
@@ -21,22 +23,43 @@ const useStyles = makeStyles((theme) => ({
 
 function Background(props) {
 	const darkMode = props.darkMode;
+	const isMobile = props.isMobile;
 	const classes = useStyles();
 
 	return (
-		<div className={classes.container}>
-			{darkMode ? (
-				<img
-					alt="City background"
-					className={classes.background}
-					src={BackgroundDark}
-				/>
+		<div>
+			{isMobile ? (
+				<div className={classes.container}>
+					{darkMode ? (
+						<img
+							alt="City background"
+							className={classes.background}
+							src={BackgroundDarkMobile}
+						/>
+					) : (
+						<img
+							alt="City background"
+							className={classes.background}
+							src={BackgroundLightMobile}
+						/>
+					)}
+				</div>
 			) : (
-				<img
-					alt="City background"
-					className={classes.background}
-					src={BackgroundLight}
-				/>
+				<div className={classes.container}>
+					{darkMode ? (
+						<img
+							alt="City background"
+							className={classes.background}
+							src={BackgroundDark}
+						/>
+					) : (
+						<img
+							alt="City background"
+							className={classes.background}
+							src={BackgroundLight}
+						/>
+					)}
+				</div>
 			)}
 		</div>
 	);
