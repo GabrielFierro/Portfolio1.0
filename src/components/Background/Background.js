99fd37a6af5dd837021d9b3c375e45d66aa65088
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-07-12 18:54:03 -0300

    add: add the properties in a internal style for the background

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,23 +1,41 @@
+/* import from MateriaulUI */
+import { makeStyles } from "@material-ui/core";
 /* Background.js */
 import "./Background.css";
 import BackgroundLight from "../../assets/images/background/city-light-desktop.jpg";
 import BackgroundDark from "../../assets/images/background/city-dark-desktop.jpg";
 
+const useStyles = makeStyles((theme) => ({
+	container: {
+		height: "100vh",
+		objectFit: "contain",
+		width: "100%",
+	},
+	background: {
+		height: "100vh",
+		objectFit: "cover",
+		position: "relative",
+		width: "100%",
+		zIndex: "-1",
+	},
+}));
+
 function Background(props) {
 	const darkMode = props.darkMode;
+	const classes = useStyles();
 
 	return (
-		<div className="container">
+		<div className={classes.container}>
 			{darkMode ? (
 				<img
 					alt="City background"
-					className="background"
+					className={classes.background}
 					src={BackgroundDark}
 				/>
 			) : (
 				<img
 					alt="City background"
-					className="background"
+					className={classes.background}
 					src={BackgroundLight}
 				/>
 			)}
